8dcf20961512801607bc11e7dc77af6deeca0a6a
Jack Gordon <appjackstudio@gmail.com>
2022-03-08 18:29:25 -0600

    Refresh spotify token on expiry

--- bot/src/spotify-api.ts
+++ bot/src/spotify-api.ts
@@ -1,5 +1,6 @@
 import SpotifyWebApi from 'spotify-web-api-node'
 import NodeCache from 'node-cache'
+import retry from 'async-retry'
 const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } = process.env as Record<string, string>;
 
 const cacheOpts: NodeCache.Options = {
@@ -12,7 +13,19 @@ const spotifyClient = new SpotifyWebApi({
     clientId: SPOTIFY_CLIENT_ID,
     clientSecret: SPOTIFY_CLIENT_SECRET
 })
-spotifyClient.setAccessToken((await spotifyClient.clientCredentialsGrant()).body.access_token)
+
+const REFRESH_ON_REMAINING = 600;
+
+async function refreshAccessToken() {
+    const { body } = await retry(() => spotifyClient.clientCredentialsGrant(), {
+        forever: true, onRetry: console.warn, maxTimeout: REFRESH_ON_REMAINING * 1000
+    })
+    spotifyClient.setAccessToken(body.access_token)
+    console.log(`Refreshed spotify access token, expires in ${body.expires_in} seconds`)
+    setTimeout(refreshAccessToken, (body.expires_in - REFRESH_ON_REMAINING) * 1000);
+}
+
+await refreshAccessToken()
 
 export async function search(query: string) {
     const { body } = await spotifyClient.search(query, ['track', 'album', 'playlist'])
