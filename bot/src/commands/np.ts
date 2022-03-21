b190261ee69f88d6da6a146814e9702ef4cbd79f
Jack Gordon <appjackstudio@gmail.com>
2022-03-21 00:44:31 -0500

    i'm tired

--- bot/src/commands/np.ts
+++ bot/src/commands/np.ts
@@ -8,7 +8,7 @@ export default class NowPlayingCommand extends Command {
 
         if (subscription.nowPlaying) {
             await interaction.reply({
-                content: t('command.np.response.success', subscription.nowPlaying),
+                content: t('command.np.response.success', subscription.nowPlaying.info),
                 embeds: [await subscription.nowPlaying.generateEmbed()]
             })
         } else {
